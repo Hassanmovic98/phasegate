9ffa950edb7a681e3aae0e47db7d19560fca81d5
Hassanmovic98 <salakohassanmobolaji@gmail.com>
2025-06-17 17:37:05 +0100

    task3

---
+++ FIREDRILL1/JAVA/Task3.java
@@ -0,0 +1,18 @@
+public class Task3 {
+    public static void main(String[] args) {
+       
+     System.out.println("Odd numbers:");
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i);
+            }
+        }
+
+
+
+
+
+
+ }
+
+}
