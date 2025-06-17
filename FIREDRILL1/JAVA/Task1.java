347d9f21b1baf8dc73fb083640cd5b2256d76fb9
Hassanmovic98 <salakohassanmobolaji@gmail.com>
2025-06-17 17:35:15 +0100

    task1

---
+++ FIREDRILL1/JAVA/Task1.java
@@ -0,0 +1,25 @@
+public class Task1 {
+	public static void main (String [] args) {
+
+	for (int number = 0; number < 11 ; number++) {
+		System.out.println(number);
+            }	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+}
