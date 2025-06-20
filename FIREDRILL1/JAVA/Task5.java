f239468a8fedbe3f3f0dd612f3ecc550c9c098d9
Hassanmovic98 <salakohassanmobolaji@gmail.com>
2025-06-20 08:25:14 +0100

    loop

---
+++ FIREDRILL1/JAVA/Task5.java
@@ -0,0 +1,20 @@
+public class Task5 {
+	public static void main(String[] args) {
+		
+	for(int i = 1; i <= 10; i++) {
+
+		if(i % 4 == 0) {
+
+			for(int j = 0; j < 5; j++) {  
+	
+			System.out.println(i);
+			}
+
+		}
+	
+
+	}
+
+
+	}
+}
